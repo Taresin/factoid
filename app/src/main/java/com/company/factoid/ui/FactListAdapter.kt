00bf5d97f80066ac52c8967475392c9e77cc4117
SalicBlu3 <dougy_style@hotmail.com>
2020-04-18 21:11:04 +1000

    Fix laggy experience when flinging through recyclerview

--- app/src/main/java/com/company/factoid/ui/FactListAdapter.kt
+++ app/src/main/java/com/company/factoid/ui/FactListAdapter.kt
@@ -7,10 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.ImageViewTarget
+import com.bumptech.glide.request.transition.Transition
 import com.company.factoid.R
 import com.company.factoid.model.Fact
 import com.company.factoid.utils.presentText
@@ -33,39 +32,41 @@ class FactListAdapter() :
     }
 
     class FactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView = itemView.image
+        val target by lazy {
+            object : ImageViewTarget<Drawable>(imageView) {
+                override fun setResource(resource: Drawable?) {
+                    imageView.setImageDrawable(resource)
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable,
+                    transition: Transition<in Drawable>?
+                ) {
+                    super.onResourceReady(resource, transition)
+                    imageView.visibility = View.VISIBLE
+                }
+
+                override fun onLoadFailed(errorDrawable: Drawable?) {
+                    super.onLoadFailed(errorDrawable)
+                    imageView.visibility = View.GONE
+                }
+            }
+        }
+
         fun bind(fact: Fact) {
             itemView.title.presentText(fact.title)
             itemView.description.presentText(fact.description)
-
+            itemView.image.visibility = View.VISIBLE
             val imageUrl = fact.imageHref
-            itemView.image.visibility = if (imageUrl.isNullOrBlank()) View.GONE else View.VISIBLE
-
-            imageUrl?.let {
-                Glide.with(itemView.context)
-                    .load(Uri.parse(it))
-                    .addListener(object : RequestListener<Drawable> {
-                        override fun onLoadFailed(
-                            e: GlideException?,
-                            model: Any?,
-                            target: Target<Drawable>?,
-                            isFirstResource: Boolean
-                        ): Boolean {
-                            itemView.image.visibility = View.GONE
-                            return false
-                        }
-
-                        override fun onResourceReady(
-                            resource: Drawable?,
-                            model: Any?,
-                            target: Target<Drawable>?,
-                            dataSource: DataSource?,
-                            isFirstResource: Boolean
-                        ): Boolean {
-                            itemView.image.visibility = View.VISIBLE
-                            return false
-                        }
-                    }).into(itemView.image)
-
+            if (imageUrl == null) {
+                imageView.setImageDrawable(null)
+                imageView.visibility = View.GONE
+            } else {
+                Glide.with(imageView)
+                    .load(Uri.parse(imageUrl))
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(target)
             }
         }
     }
