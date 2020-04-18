db760360a2be355fad47e4202aae843067daa9e3
SalicBlu3 <dougy_style@hotmail.com>
2020-04-18 19:03:05 +1000

    Show/Hide image view if error or url is empty/blank

--- app/src/main/java/com/company/factoid/ui/FactListAdapter.kt
+++ app/src/main/java/com/company/factoid/ui/FactListAdapter.kt
@@ -1,11 +1,16 @@
 package com.company.factoid.ui
 
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.company.factoid.R
 import com.company.factoid.model.Fact
 import com.company.factoid.utils.presentText
@@ -16,7 +21,8 @@ class FactListAdapter(private val list: List<Fact>) :
     override fun getItemCount(): Int = list.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FactViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_fact, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item_fact, parent, false)
         return FactViewHolder(view)
     }
 
@@ -29,11 +35,35 @@ class FactListAdapter(private val list: List<Fact>) :
             itemView.title.presentText(fact.title)
             itemView.description.presentText(fact.description)
 
-            fact.imageHref?.let {
+            val imageUrl = fact.imageHref
+            itemView.image.visibility = if (imageUrl.isNullOrBlank()) View.GONE else View.VISIBLE
+
+            imageUrl?.let {
                 Glide.with(itemView.context)
                     .load(Uri.parse(it))
-                    .into(itemView.image)
-                println("Loading: $it")
+                    .addListener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            itemView.image.visibility = View.GONE
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            itemView.image.visibility = View.VISIBLE
+                            return false
+                        }
+                    }).into(itemView.image)
+
             }
         }
     }
